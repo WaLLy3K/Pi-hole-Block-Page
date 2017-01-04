2d8647766c5a12823cb312ddc8c21db30ba9ae79
WaLLy3K <github@firebog.net>
2017-01-05 00:01:44 +1100

    Minor updates
    * Do not show primary flag if user doesn't have their list set within config
    * Cleaned up code

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-# Pi-hole Block Page: Show "Website Blocked" on blacklisted domains
-# by WaLLy3K 06SEP16 for Pi-hole
+// Pi-hole Block Page: Show "Website Blocked" on blacklisted domains
+// by WaLLy3K 06SEP16 for Pi-hole
 
-# Define "flagType" of indivudual adlists.list URLs
-# TODO: This could be done better
+// Define "flagType" of indivudual adlists.list URLs
+// TODO: This could be done better
 $suspicious = array(
   "raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt",
   "raw.githubusercontent.com/StevenBlack/hosts/master/hosts",
@@ -69,93 +69,90 @@ $malicious = array(
   "raw.githubusercontent.com/StevenBlack/hosts/master/data/add.Risk/hosts",
 );
 
-# External config options outside WWW/HTML folder
-if (file_exists("/var/phbp.php")) include '/var/phbp.php';
+// External config options outside WWW/HTML folder
+if (file_exists("/var/phbp.php")) include "/var/phbp.php";
 
-# Merge custom flagTypes with default flagTypes
+// Merge custom flagTypes with default flagTypes
 if (isset($suspicious_custom)) $suspicious = array_merge($suspicious, $suspicious_custom);
 if (isset($advertising_custom)) $advertising = array_merge($advertising, $advertising_custom);
 if (isset($tracking_custom)) $tracking = array_merge($tracking, $tracking_custom);
 if (isset($malicious_custom)) $malicious = array_merge($malicious, $malicious_custom);
 
-# Default Config Options
-if (!isset($css)) $css = "https://wally3k.github.io/style/pihole.css"; # Default CSS
-if (!isset($favicon)) $favicon = "/admin/img/favicon.png"; # Default Favicon
-if (!isset($logo)) $logo = "https://wally3k.github.io/style/phv.svg"; # Default Logo
-if (!isset($blockedImage)) $blockImage = "https://wally3k.github.io/style/blocked.svg"; # Default Block Image
+// Default Config Options
+if (!isset($css)) $css = "https://wally3k.github.io/style/pihole.css"; // Default CSS
+if (!isset($favicon)) $favicon = "/admin/img/favicon.png"; // Default Favicon
+if (!isset($logo)) $logo = "https://wally3k.github.io/style/phv.svg"; // Default Logo
+if (!isset($blockedImage)) $blockImage = "https://wally3k.github.io/style/blocked.svg"; // Default Block Image
 
-# Define which URL extensions get rendered as "Website Blocked"
-# Index files should always be rendered as "Website Blocked" anyway
-$webRender = array('asp', 'htm', 'html', 'php', 'rss', 'xml');
+// Define which URL extensions get rendered as "Website Blocked"
+// Index files should always be rendered as "Website Blocked" anyway
+$webRender = array("asp", "htm", "html", "php", "rss", "xml");
 
-# "Should" prevent arbitrary commands from being run as www-data when using wget
+// "Should" prevent arbitrary commands from being run as www-data when using wget
 $serverName = escapeshellcmd($_SERVER['SERVER_NAME']);
 
-# Retrieve server URI extension (EG: jpg, exe, php)
+// Retrieve server URI extension (EG: jpg, exe, php)
 $uriExt = pathinfo($_SERVER['REQUEST_URI'], PATHINFO_EXTENSION);
 
-# Define URI types
+// Handle type of block page
 if ($serverName == "pi.hole") {
-  header('Location: admin');
+  header("Location: admin");
 }elseif (!empty($landPage) && $serverName == $_SERVER['SERVER_ADDR'] || !empty($landPage) && $serverName == $selfDomain) {
-  # When browsing to RPi, redirect to custom landing page
+  // When browsing to RPi, redirect to custom landing page
   include $landPage;
   exit();
 }elseif (in_array($uriExt, $webRender)) {
-  $uriType = "site";
+  // Valid URL extension to render as "Website Blocked"
 }elseif (substr_count($_SERVER['REQUEST_URI'], "?") && isset($_SERVER['HTTP_REFERER'])) {
-  # Serve a 1x1 blank gif to POTENTIAL iframe
+  // Serve a 1x1 blank gif to POTENTIAL iframe with query string
   die('<img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7">');
 }elseif (!empty($uriExt) || substr_count($_SERVER['REQUEST_URI'], "?")) {
-  # If file extension, or non-iframed query string
-  # Serve this image to URI's defined as file
+  // Invalid URL extension or non-iframed query string
   die('<head><meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/></head><img src="'.$blockImage.'"/>');
-}else{
-  $uriType = "site";
 }
 
-# Some error handling
-if (empty(glob('/etc/pihole/*domains'))) die("[ERROR]: There are no blacklists in the Pi-hole folder! Please update the list of ad-serving domains.");
+// Some error handling
+if (empty(glob("/etc/pihole/*domains"))) die("[ERROR]: There are no blacklists in the Pi-hole folder! Please update the list of ad-serving domains.");
 if (!file_exists("/etc/pihole/adlists.list")) die("[ERROR]: There is no 'adlists.list' in the Pi-hole folder!");
 
-# Exact search, returning a numerically sorted # list of matching .domains
+// Get all URLs starting with "http" from adlists.list
+// $urlList array key expected to match .domains list // in $listMatches!!
+// This may not work if admin updates gravity, and later inserts a new hosts URL at anywhere but the end before re-running gravity
+$urlList = array_values(preg_grep("/(^http)|(^www)/i", file("/etc/pihole/adlists.list", FILE_IGNORE_NEW_LINES)));
+
+// Strip any combo of HTTP, HTTPS and WWW
+$urlList_match = preg_replace("/https?\:\/\/(www.)?/i", "", $urlList);
+
+// Exact search, returning a numerically sorted # list of matching .domains
 exec('wget -qO - "http://pi.hole/admin/scripts/pi-hole/php/queryads.php?domain="'.$serverName.'"&exact" | grep -E ".domains.*\([1-9]" | cut -d. -f2 | sort -un', $listMatches);
 
-# Get all URLs starting with "http" from adlists.list
-# $urlList array key expected to match .domains list # in $listMatches!!
-# This may not work if admin updates gravity, and later inserts a new hosts URL at anywhere but the end before re-running gravity
-$urlList = array_values(preg_grep("/(^http)|(^www)/i", file('/etc/pihole/adlists.list', FILE_IGNORE_NEW_LINES)));
-
-# Strip any combo of HTTP, HTTPS and WWW
-$urlList_match = preg_replace('/https?\:\/\/(www.)?/i', '', $urlList);
-
-# Return how many lists URL is featured in, and total lists count
+// Return how many lists URL is featured in
 $featuredTotal = count(array_values(array_unique($listMatches)));
-$totalLists = count($urlList);
 
-# Featured total will be 0 for a manually blacklisted site
+// Featured total will be 0 for a manually blacklisted site
 if ($featuredTotal == "0") {
     $notableFlag = "Blacklisted manually";
 }else{
   $in = NULL;
-  # Define "Featured Flag"
+  // Define "Featured Flag"
   foreach ($listMatches as $num) {
-    # Create a string of flags for URL
+    // Create a string of flags for URL
     if(in_array($urlList_match[$num], $suspicious)) $in .= "sus ";
     if(in_array($urlList_match[$num], $advertising)) $in .= "ads ";
     if(in_array($urlList_match[$num], $tracking)) $in .= "trc ";
     if(in_array($urlList_match[$num], $malicious)) $in .= "mal ";
     
-    # Return value of worst flag to user (EG: Malicious more notable than Suspicious)
+    // Return value of worst flag to user (EG: Malicious more notable than Suspicious)
     if (substr_count($in, "sus")) $notableFlag = "Suspicious";
     if (substr_count($in, "ads")) $notableFlag = "Advertising";
     if (substr_count($in, "trc")) $notableFlag = "Tracking & Telemetry";
     if (substr_count($in, "mal")) $notableFlag = "Malicious";
-    if (empty($in)) $notableFlag = "Unspecified Flag";
+    
+    // Do not show primary flag if we are unable to find one
+    if (empty($in)) $notableFlag = "-1";
   }
 }
 ?>
-
 <!DOCTYPE html><head>
   <meta charset='UTF-8'/>
   <title>Website Blocked</title>
@@ -174,7 +171,6 @@ if ($featuredTotal == "0") {
         e.style.display = 'block';
         document.getElementById("info").innerHTML = "Less Info";
       }
-      
     }
   </script>
   <style>
@@ -183,36 +179,36 @@ if ($featuredTotal == "0") {
 </head><body><header>
   <h1><a href='/'>Website Blocked</a></h1>
 </header><main>
-
   <div class="url">
     Access to the following site has been blocked:
     <span class="msg"><?php echo $serverName; ?></span>
   </div>
+  <?php if ($notableFlag !== "-1") { ?>
   <div class="flag">
     This is primarily due to being flagged as:
     <span class='msg'><?php echo $notableFlag; ?></span>
   </div>
+  <?php } ?>
   <div class="notice">
     If you have an ongoing use for this website, please <a href='<?php echo "mailto:$adminEmail?subject=Site Blocked: $serverName"; ?>'>ask to have it whitelisted</a>.
   </div>
-  
   <div class='buttons'>
     <a id='back' href='javascript:history.back()'>Back to safety</a>
     <?php if ($featuredTotal != "0") echo "<a id='info' onclick='tgVis(\"querylist\");'>More Info</a>"; ?>
   </div> 
-  <div id='querylist' style='display: none;'>This site is found in <?php echo "$featuredTotal of $totalLists"; ?> lists:
+  <div id='querylist'>This site is found in <?php echo "$featuredTotal of ".count($urlList); ?> lists:
     <pre id='output'><?php foreach ($listMatches as $num) { echo "[$num]:\t<a href='$urlList[$num]'>$urlList[$num]</a><br/>"; } ?></pre>
-    <?php if ($allowWhitelisting == "true") { echo "
+    <?php if ($allowWhitelisting == "true") { ?>
     <form class='buttons'>
-      <input id='domain' value='$serverName' disabled>
+      <input id='domain' value='<?php echo $serverName; ?>' disabled>
       <input type='password' id='pw' name='pw' placeholder='Pi-hole Password'/>
       <button id='whitelist' type='button'>Whitelist</button>
      </form>
      <pre id='notification' hidden='true'></pre>
-     ";} ?>
+     <?php } ?>
   </div>
 </main>
-<footer>Generated <?php echo date('D g:i A, M d'); ?> by <a href='https://github.com/WaLLy3K/Pi-hole-Block-Page'>Pi-hole Block Page</a></footer>
+<footer>Generated <?php echo date("D g:i A, M d"); ?> by <a href='https://github.com/WaLLy3K/Pi-hole-Block-Page'>Pi-hole Block Page</a></footer>
 <script>
   function add() {
     var domain = $("#domain");
@@ -242,6 +238,7 @@ if ($featuredTotal == "0") {
       }
     });
   }
+
   // Handle enter button for adding domains
   $(document).keypress(function(e) {
       if(e.which === 13 && $("#pw").is(":focus")) {
